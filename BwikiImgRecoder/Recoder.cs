5aede8a57053eae39c7da01c22f391d51b0d040c
norara <792167485@qq.com>
2025-01-01 22:31:09 +0800

    Update Recoder.cs

--- BwikiImgRecoder/Recoder.cs
+++ BwikiImgRecoder/Recoder.cs
@@ -1,11 +1,14 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 using OpenCvSharp;
+using CvSize = OpenCvSharp.Size;
 
 namespace BwikiImgRecoder;
 
 class Recoder
 {
     private static readonly BigInteger MAX_LENGTH = new BigInteger(1024 * 1024) * 8;
+    private static readonly int STEPPING = 20;
     static void Main(string[] args)
     {
         if (args.Length < 1)
@@ -33,46 +36,83 @@ class Recoder
         
         string targetPath = Directory.CreateDirectory($"Result_{DateTime.Now:yyyy-dd-M--HH-mm-ss}").FullName;
         var genFilename = (string targetDic ,string filename) => Path.Combine($"{targetDic}", $"{DateTime.Now:yyyy-dd-M--HH-mm-ss}_{filename}");
-
+        Console.WriteLine($"Start...");
+        Console.WriteLine($"Output file path: {targetPath}");
+        
         foreach (var x in fileList)
         {
             string filename = Path.GetFileName(x);
             var fi = new FileInfo(x);
+            
+            Console.WriteLine("☆ = = = = = = = = ");
+            Console.WriteLine($"Now processing file {fi.Name}");
+            
+            Stopwatch sw = Stopwatch.StartNew();
+            
             if (fi.Length <= MAX_LENGTH)
             {
                 var targetFilename = genFilename(targetPath, filename);
-                Console.WriteLine($"[{filename}], {Math.Round((decimal)fi.Length/1024/1024, 2)} MB, CPY  -> {targetFilename}");
-                File.Copy(x, targetFilename, true);
-                continue;
-            }
-            
-            var sourceMat = Cv2.ImRead(x);
-            Cv2.ImEncode(ext, sourceMat, out byte[] encodedImg);
-            
-            if (encodedImg.Length <= MAX_LENGTH)
-            {
-                filename = $"{Path.GetFileNameWithoutExtension(x)}{ext}";
-                var targetFilename = genFilename(targetPath, filename);
-                Console.WriteLine($"[{filename}], {Math.Round((decimal)fi.Length/1024/1024, 2)} MB, CPS1  -> {targetFilename}");
-                File.WriteAllBytes(targetFilename, encodedImg);
-                sourceMat.Dispose();
-    
-            }
-            else
-            {
-                var result = Compress(sourceMat, ext);
-                var targetFilename = genFilename(targetPath, filename);
-                filename = $"{Path.GetFileNameWithoutExtension(x)}{ext}";
-                Console.WriteLine($"[{filename}], {Math.Round((decimal)fi.Length/1024/1024, 2)} MB, CPS2  -> {targetFilename}");
-                File.WriteAllBytes(genFilename(targetPath, filename), result);
-             
-            }
-            
-        }
+                try
+                {
+                    File.Copy(x, targetFilename, true);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed to copy file {x} to {targetFilename}: {e.Message}");
+                    continue;
+                }
 
+                Console.WriteLine($"[{filename}], {Math.Round((decimal)fi.Length/1024/1024, 2)} MB, CPY");
+                sw.Stop();
+                Console.WriteLine($"★ Success. {sw.ElapsedMilliseconds}ms = = = = ");
+                
+                continue;
+                
+            }
+
+            try
+            {
+                var sourceMat = Cv2.ImRead(x);
+                Cv2.ImEncode(ext, sourceMat, out byte[] encodedImg);
+
+                if (encodedImg.Length <= MAX_LENGTH)
+                {
+                    filename = $"{Path.GetFileNameWithoutExtension(x)}{ext}";
+                    var targetFilename = genFilename(targetPath, filename);
+ 
+                    File.WriteAllBytes(targetFilename, encodedImg);
+                    Console.WriteLine(
+                        $"[{filename}], {Math.Round((decimal)fi.Length / 1024 / 1024, 2)} MB, CPS1");
+                }
+                else
+                {
+
+                    var result = Compress(sourceMat, ext,
+                        new CvSize() { Width = sourceMat.Width / STEPPING, Height = sourceMat.Height / STEPPING });
+                    var targetFilename = genFilename(targetPath, filename);
+                    filename = $"{Path.GetFileNameWithoutExtension(x)}{ext}";
+                    File.WriteAllBytes(genFilename(targetPath, filename), result);
+                    Console.WriteLine(
+                        $"[{filename}], {Math.Round((decimal)fi.Length / 1024 / 1024, 2)} MB, CPS2");
+                }
+                sw.Stop();
+                Console.WriteLine($"★ Success. {sw.ElapsedMilliseconds}ms = = = = ");
+
+                sourceMat.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Cv2 Error: {e.Message}");
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine($"Over...");
     }
 
-    static byte[] Compress(Mat src, string ext)
+    static byte[] Compress(Mat src, string ext, CvSize s)
     {
         Cv2.ImEncode(ext, src, out byte[] encodedImg);
         if (encodedImg.Length <= MAX_LENGTH)
@@ -84,14 +124,14 @@ class Recoder
         if (encodedImg.Length >= MAX_LENGTH * 2)
         {
 
-            Cv2.Resize(src, newMat, new Size(src.Width*0.75, src.Height*0.75));
+            Cv2.Resize(src, newMat, new CvSize(src.Width*0.75, src.Height*0.75));
             src.Dispose();
-            return Compress(newMat, ext);
+            return Compress(newMat, ext, s);
         }
         
-        Cv2.Resize(src, newMat, new Size(src.Width*0.9, src.Height*0.9));
+        Cv2.Resize(src, newMat, new CvSize(src.Width - s.Width, src.Height - s.Height));
         src.Dispose();
-        return Compress(newMat, ext);
+        return Compress(newMat, ext, s);
     }
     
 }
